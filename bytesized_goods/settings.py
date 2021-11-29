67aa2083d84dc663c23f36cf004cc45db70a22a5
SpudStar <ed504@sky.com>
2021-11-29 22:01:52 +0000

    Add structure for a pokemon to be displayed

--- bytesized_goods/settings.py
+++ bytesized_goods/settings.py
@@ -100,6 +100,9 @@ LOGIN_REDIRECT_URL = '/success'
 
 WSGI_APPLICATION = 'bytesized_goods.wsgi.application'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -149,6 +152,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
