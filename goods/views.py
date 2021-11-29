2ec70849f98ced59c22faa96388a5b118a6006df
SpudStar <ed504@sky.com>
2021-11-29 20:51:03 +0000

    Made base preperations for product display page, cant get it to work yet

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Info
 
 # Create your views here.
+
+def all_products(request):
+
+    data = Info.objects.all()
+
+    context = {
+        'products': data,
+    }
+
+    return render(request, 'products/products.html', context)
