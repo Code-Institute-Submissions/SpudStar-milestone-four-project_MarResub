c61bc973b2638043125a8b15acf7c0f31b024d91
SpudStar <ed504@sky.com>
2021-11-29 19:56:09 +0000

    Add JSON fixtures containing pokemon data

--- goods/models.py
+++ goods/models.py
@@ -13,15 +13,20 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=126, null=True, blank=True)
-    name = models.CharField(max_length=126)
-    description = models.TextField()
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+class Info(models.Model):
+    abilities = models.CharField(max_length=30)
+    type1 = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    type2 = models.CharField(max_length=20)
+    attack = models.IntegerField(default=1)
+    defense = models.IntegerField(default=1)
+    hp = models.IntegerField(default=1)
+    name = models.CharField(max_length=20)
+    sp_attack = models.IntegerField(default=1)
+    sp_defense = models.IntegerField(default=1)
+    speed = models.IntegerField(default=1)
+    generation = models.IntegerField(default=1)
+    level = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.name
