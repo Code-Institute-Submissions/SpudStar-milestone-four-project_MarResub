7f9f6b9d11339511f1d488cc0d45e8eac2a2570c
SpudStar <ed504@sky.com>
2022-01-12 16:14:47 +0000

    Add functionality to search by type using category buttons

--- goods/views.py
+++ goods/views.py
@@ -9,8 +9,14 @@ def all_products(request):
 
     products = Info.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            queries = Q(type1__in=categories) | Q(type2__in=categories)
+            products = products.filter(queries)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
