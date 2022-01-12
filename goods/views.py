55b2507e6e974195644e50f47fc808f88cf97308
SpudStar <ed504@sky.com>
2022-01-12 15:08:05 +0000

    Add search by name functionality to the searchbar

--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Info
 
 # Create your views here.
@@ -6,9 +8,21 @@ from .models import Info
 def all_products(request):
 
     products = Info.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No Pokemon name entered")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) 
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
