99985adbc50f89ca6fae8b2bbc7e29fb0288b3a2
SpudStar <ed504@sky.com>
2022-01-13 18:12:37 +0000

    Fixed issue with contexts.py causing errors, forgot I had to use a dictionary

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,6 +5,9 @@ def bag_contents(request):
     bag_items = []
     product_count = 0
 
-    context = {bag_items, product_count}
+    context = {
+        'bag_items': bag_items,
+        'product_count': product_count,
+        }
 
     return context
