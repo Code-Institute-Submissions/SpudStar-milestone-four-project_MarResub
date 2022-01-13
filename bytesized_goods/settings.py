56d1ed7847a75e82bf2cd1c21a00c7056b374e62
SpudStar <ed504@sky.com>
2022-01-13 17:26:24 +0000

    Added file called contexts.py to hold which pokemon have been selected by the user, added context in settings.py to allow full template access

--- bytesized_goods/settings.py
+++ bytesized_goods/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',  # Used by Allauth
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -151,8 +152,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
