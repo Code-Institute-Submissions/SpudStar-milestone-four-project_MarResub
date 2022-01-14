22b5b5f284288a28bb617abd4c83d77752208bc0
SpudStar <ed504@sky.com>
2022-01-14 21:07:07 +0000

    Added code to direct the user if checkout is sucessful, having issue with Info.objects however

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 
+from goods.models import Info
+from .models import Order, OrderLineItem
 from bag.contexts import SUBSCRIPTION_COST
 from .forms import OrderForm
 
@@ -11,23 +13,68 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
-    if not bag:
-        return redirect(reverse('products'))
 
-    order_form = OrderForm()
-
-    stripe_total = round(SUBSCRIPTION_COST*100)
-    stripe.api_key = stripe_secret_key
-
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-    )
-    
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+    if request.method == "POST":
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'user_trainer_code': request.POST['user_trainer_code'],
         }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
 
-    return render(request, 'checkout/checkout.html', context)
+            for item_id, item_data in bag.items():
+                try:
+                    product = Info.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                    )
+                    order_line_item.save()
+
+                except Info.DoesNotExist:
+                    # Error Message here
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+            
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            # Error Message
+            temp = 0
+
+    else:
+        if not bag:
+            return redirect(reverse('products'))
+
+        order_form = OrderForm()
+
+        stripe_total = round(SUBSCRIPTION_COST*100)
+        stripe.api_key = stripe_secret_key
+
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
+
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+            }
+
+        return render(request, 'checkout/checkout.html', context)
+
+def checkout_success(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    # Insert Success Message
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    context = {
+        'order': order,
+    }
+
+    return render(request, 'checkout/checkout_success.html', context)
