9f075f4b06827adab8cc351c77879b186e55deea
SpudStar <ed504@sky.com>
2022-01-14 20:05:17 +0000

    Set up secret and public key variables for stripe in the enviroment, and used it to get the intent of the stripe purchase

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404
 from goods.models import Info
 
+SUBSCRIPTION_COST = 4.99
+
 def bag_contents(request):
 
     bag_items = []
     product_count = 0
+    
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
