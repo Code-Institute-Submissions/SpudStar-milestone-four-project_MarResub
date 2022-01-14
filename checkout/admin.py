65d491d858c34e68788ab5bd89134d3aa6a3ea0b
SpudStar <ed504@sky.com>
2022-01-14 09:28:15 +0000

    Did some setup for the admin fields for Orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Order, OrderLineItem
+
+class OrderAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('order_number', 'date',)
+
+    fields = ('order_number', 'date', 'email', 'full_name', 'user_trainer_code')
+
+    list_display = ('order_number', 'date', 'full_name',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
