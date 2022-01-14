c446072ea147952cdb600fe520acc708fa744e1c
SpudStar <ed504@sky.com>
2022-01-14 10:51:28 +0000

    Add updates of bag quantity for the user

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,9 +1,19 @@
-
+from django.shortcuts import get_object_or_404
+from goods.models import Info
 
 def bag_contents(request):
 
     bag_items = []
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Info, pk=item_id)
+        product_count += 1
+        bag_items.append({
+            'item_id': item_id,
+            'product': product,
+        })
 
     context = {
         'bag_items': bag_items,
