9a28dc18b69061d5cf61534e9c32112f4e3362e6
SpudStar <ed504@sky.com>
2022-01-14 16:33:36 +0000

    Add stripe and card element to checkout, fix STATIC_URL

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,10 @@ def checkout(request):
         return redirect(reverse('products'))
 
     order_form = OrderForm()
-    context = {'order_form': order_form}
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KHrw6AhGjWDCyMj1IR36w0iEB8YpJH7YMrPtB502Fu8Dt3FszbjxLfbv1CxyTUOSypv0RDlWKfn2xylyr8Pq7Hy00BgpF1aJk',
+        'client_secret': 'secret',
+        }
 
     return render(request, 'checkout/checkout.html', context)
