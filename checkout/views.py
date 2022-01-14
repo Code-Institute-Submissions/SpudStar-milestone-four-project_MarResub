748c0a33d96f085dfdb1ff03a3c674d016e1c55e
SpudStar <ed504@sky.com>
2022-01-14 14:42:45 +0000

    Updated the checkout views.py to allow for the order form context to be sent, fixed incorrect link to template too

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
+from .forms import OrderForm
 # Create your views here.
 
 def checkout(request):
-    return render(request, 'checkout/index.html')
+
+    bag = request.session.get('bag', {})
+    if not bag:
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    context = {'order_form': order_form}
+
+    return render(request, 'checkout/checkout.html', context)
