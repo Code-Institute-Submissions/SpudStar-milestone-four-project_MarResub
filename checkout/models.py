3d2957b8c559cb5a81c965de0d462411751184ec
SpudStar <ed504@sky.com>
2022-01-14 09:28:15 +0000

    Add setup to save orders in the admin

--- checkout/models.py
+++ checkout/models.py
@@ -12,9 +12,22 @@ class Order(models.Model):
     full_name = models.CharField(max_length=64, null=False, blank=False)
     email = models.EmailField(max_length=255, null=False, blank=False)
     user_trainer_code = models.CharField(max_length=255, null=False, blank=False)
-    total_requests = models.IntegerField(max_length=16, null=False, default=0)
     date = models.DateTimeField(auto_now_add=True)
+
+    def _generate_order_number(self):
+
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name="lineitems")
-    
+    #NEED SKU RETURN HERE, CHECKOUT2
