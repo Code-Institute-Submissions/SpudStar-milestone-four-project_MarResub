da332ab7126c323bbdc3468a3f5a377e4130ee23
SpudStar <ed504@sky.com>
2022-01-15 09:54:44 +0000

    Add template for adding a product with form fields

--- checkout/models.py
+++ checkout/models.py
@@ -12,10 +12,12 @@ class Order(models.Model):
     order_number = models.CharField(max_length=16, null=False, editable=False)
     full_name = models.CharField(max_length=64, null=False, blank=False)
     email = models.EmailField(max_length=255, null=False, blank=False)
-    user_trainer_code = models.CharField(max_length=255, null=False, blank=False)
+    user_trainer_code = models.CharField(max_length=255,
+                                         null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                                     null=True, blank=True, related_name='orders')
+                                     null=True, blank=True,
+                                     related_name='orders')
 
     def _generate_order_number(self):
 
@@ -32,8 +34,11 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Info, null=False, blank=False, on_delete=models.CASCADE, default="")
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    product = models.ForeignKey(Info, null=False, blank=False,
+                                on_delete=models.CASCADE, default="")
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
--- goods/views.py
+++ goods/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Info
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -43,3 +44,12 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+
+    form = ProductForm()
+    template = "products/add_product.html"
+    context = {
+        'form': form
+    }
+    return render(request, template, context)
