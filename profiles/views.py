b7f3f38a157d7e321010518a483b6f8601a3556d
SpudStar <ed504@sky.com>
2022-01-15 14:15:27 +0000

    Style Allauth templates using django crispy forms

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
@@ -7,7 +6,7 @@ from .models import UserProfile
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
-
+    print(profile)
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
