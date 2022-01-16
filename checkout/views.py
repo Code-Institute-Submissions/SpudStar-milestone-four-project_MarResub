ba35af3def3b97a8e57338e3d636f3ede27e7f9e
SpudStar <ed504@sky.com>
2022-01-16 05:58:34 +0000

    Improved checkout.html, fixed indentation problem

--- checkout/views.py
+++ checkout/views.py
@@ -42,7 +42,6 @@ def checkout(request):
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success',
                             args=[order.order_number]))
-        else:
     else:
         bag = request.session.get('bag', {})
         if not bag:
