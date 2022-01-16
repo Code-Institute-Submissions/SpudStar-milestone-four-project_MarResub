39a84d4c989dd96ad3a498185acd12a9d3b6c518
SpudStar <ed504@sky.com>
2022-01-16 00:36:20 +0000

    Test deployment to Heroku

--- bytesized_goods/settings.py
+++ bytesized_goods/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 """
 Django settings for bytesized_goods project.
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-y)x$l1io422tl5tgeu2*f8mo=e4*i8bhsbk9o(fm&v6ffdfc*3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
  
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bytesized-trades-2.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -119,13 +120,19 @@ STATICFILES_DIRS = [
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
