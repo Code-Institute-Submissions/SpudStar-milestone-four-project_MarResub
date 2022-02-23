60f657841e43ac0c696d1ce8bc747440bc632519
SpudStar <ed504@sky.com>
2022-02-23 21:16:33 +0000

    Website now deploys, testing if collect static has been fixed as a result

--- bytesized_goods/settings.py
+++ bytesized_goods/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'project_name.settings.local')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = True
 ALLOWED_HOSTS = ['bytesized-trades-2.herokuapp.com', 'localhost']
 
