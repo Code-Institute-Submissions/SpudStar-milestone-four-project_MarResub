0f23f992ba1de84a3c1e4e59802d86044d24b6c2
SpudStar <ed504@sky.com>
2022-02-23 20:36:36 +0000

    Reverted changes of last commit, deploying without collecting static to check it is deploying

--- bytesized_goods/settings.py
+++ bytesized_goods/settings.py
@@ -172,11 +172,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if "USE_CLOUD" in os.environ:
-    AWS_BUCKET_NAME = 'bytesized-trades-bucket'
+    AWS_STORAGE_BUCKET_NAME = os.getenv('BUCKET_KEY', '')
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_DEFAULT_ACL = None
 
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
