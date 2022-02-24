efe9de92347f76841e95720271becc2df2d18ba5
SpudStar <ed504@sky.com>
2022-02-24 19:23:31 +0000

    Removed ability for guest checkout due to user story conflict

--- checkout/views.py
+++ checkout/views.py
@@ -20,8 +20,6 @@ def checkout(request):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     subscription_status = False
     template = 'checkout/checkout.html'
-    bag = request.session.get('bag', {})
-    order_form = OrderForm()
     form_data = None
 
     stripe_total = round(SUBSCRIPTION_COST*100)
@@ -46,50 +44,46 @@ def checkout(request):
             # Checks if the user is subscribed
             subscription_status = profile.subscription
 
-    # Runs the complete order code if a form has been submitted, or if 
-    # the user is already subscribed
-    if request.method == 'POST' or subscription_status:
+        # Runs the complete order code if a form has been submitted, or if 
+        # the user is already subscribed
+        if request.method == 'POST' or subscription_status:
 
-        if request.user.is_authenticated and not subscription_status:
-            # Saves the user as subscribed if logged in and not subscribed
-            profile = get_object_or_404(UserProfile, user=request.user)
-            profile.subscription = True
-            profile.save()
-        else:
-            form_data = {
-                'full_name': request.POST['full_name'],
-                'email': request.POST['email'],
-                'user_trainer_code': request.POST['user_trainer_code'],
-            }
+            if not subscription_status:
+                # Saves the user as subscribed if logged in and not subscribed
+                profile = get_object_or_404(UserProfile, user=request.user)
+                profile.subscription = True
+                profile.save()
 
-        order_form = OrderForm(form_data)
+            order_form = OrderForm(form_data)
 
-        if order_form.is_valid():
-            order = order_form.save()
-            for item_id, item_data in bag.items():
-                try:
-                    product = Info.objects.get(id=item_id)
-                    order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                        )
-                    order_line_item.save()
-                # If a pokemon somehow doesn't exist, returns to bag
-                except Info.DoesNotExist:
-                    order.delete()
-                    return redirect(reverse('view_bag'))
+            if order_form.is_valid():
+                bag = request.session.get('bag', {})
+                order = order_form.save()
+                for item_id, item_data in bag.items():
+                    try:
+                        product = Info.objects.get(id=item_id)
+                        order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                            )
+                        order_line_item.save()
+                    # If a pokemon somehow doesn't exist, returns to bag
+                    except Info.DoesNotExist:
+                        order.delete()
+                        return redirect(reverse('view_bag'))
 
-            return redirect(reverse('checkout_success',
-                            args=[order.order_number]))
-        else:
-            bag = request.session.get('bag', {})
-            if not bag:
-                return redirect(reverse('products'))
+                return redirect(reverse('checkout_success',
+                                args=[order.order_number]))
+            else:
+                bag = request.session.get('bag', {})
+                if not bag:
+                    return redirect(reverse('products'))
 
-        order_form = OrderForm()
+            order_form = OrderForm()
+    else:
+        return redirect(reverse('view_bag'))
 
     context = {
-        'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
