d7b0c496bdfe6277a4c3af5ec4b33fe60bf4a982
SpudStar <ed504@sky.com>
2022-02-24 21:22:55 +0000

    Added messages to user responses

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
-# Logic similar to Boutique Ado, no much to change
-
+from django.contrib import messages
 
 # Function that returns the current bag view
 def bag(request):
@@ -13,11 +12,12 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
+    # Checks the item isnt in the bag
     if item_id not in list(bag.keys()):
         bag[item_id] = 1
+        messages.success(request, 'Pokemon has been added to your bag.')
 
     request.session['bag'] = bag
-
     return redirect(redirect_url)
 
 
@@ -26,8 +26,10 @@ def remove_from_bag(request, item_id):
 
     bag = request.session.get('bag', {})
 
+    # Checks the item is in the bag
     if item_id in list(bag.keys()):
         bag.pop(item_id)
+        messages.success(request, 'Pokemon has been removed to from bag.')
 
     request.session['bag'] = bag
 
