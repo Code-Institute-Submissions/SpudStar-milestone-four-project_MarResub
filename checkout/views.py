Code-Institute-Submissions/SpudStar-milestone-four-project_JanResub

Fixed lack of import in checkout

checkout/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 
 from goods.models import Info
+from profiles.model import UserProfile
 from .models import Order, OrderLineItem
+
 from bag.contexts import SUBSCRIPTION_COST
 from .forms import OrderForm
 

Code-Institute-Submissions/SpudStar-milestone-four-project_MarResub

checkout/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 
 from goods.models import Info
+from profiles.models import UserProfile
 from .models import Order, OrderLineItem
+
 from bag.contexts import SUBSCRIPTION_COST
 from .forms import OrderForm
 