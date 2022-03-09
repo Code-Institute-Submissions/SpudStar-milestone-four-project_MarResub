5cb457e90e7ade0ebcb33211590266cad794655c
SpudStar <ed504@sky.com>
2022-03-09 14:58:46 +0000

    DELETE7

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -61,9 +61,7 @@ form.addEventListener('submit', function(ev) {
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
+            form.submit();
         }
     });
 });
